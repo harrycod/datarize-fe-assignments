f6c28e55f0a27610e2a91882e6f919dded01e59c
harrycod <whtngus1230@gmail.com>
2024-09-04 23:27:50 +0900

    feat: 고객 상세 클릭시 모달 열리도록 바인딩

--- apps/frontend/src/components/PurchaseCustomers.tsx
+++ apps/frontend/src/components/PurchaseCustomers.tsx
@@ -2,16 +2,25 @@ import { useState } from 'react'
 
 import PurchaseCustomersFilter from './PurchaseCustomersFilter'
 import PurcahseCustomersTable from './PurcahseCustomersTable'
+import PurchaseCustomerDetail from './PurchaseCustomerDetail'
+import Modal from '../ui/modal/Modal'
 
 const PurchaseCustomersTable = () => {
   const [search, setSearch] = useState<string>('')
   const [sortBy, setSortBy] = useState<string>('id')
+  const [selectedCustomerId, setSelectedCustomerId] = useState<number | null>(null)
 
   return (
     <div>
       <h3>구매 고객 목록</h3>
       <PurchaseCustomersFilter sortBy={sortBy} onSearchChange={setSearch} onSortChange={setSortBy} />
-      <PurcahseCustomersTable search={search} sortBy={sortBy} />
+      <PurcahseCustomersTable search={search} sortBy={sortBy} onSelectCustomer={setSelectedCustomerId} />
+
+      {selectedCustomerId && (
+        <Modal onClose={() => setSelectedCustomerId(null)}>
+          <PurchaseCustomerDetail selectedCustomerId={selectedCustomerId} />
+        </Modal>
+      )}
     </div>
   )
 }
